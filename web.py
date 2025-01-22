f77efacedcce4ae44dfc04497b33d884cd8d094d
shrid <skanuparthy21@gmail.com>
2025-01-22 13:27:17 -0800

    Started using a template

--- web.py
+++ web.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Your main HTML file
+    return render_template('index.html')  # Main page
 
+# @app.route('/left-sidebar')
+# def left_sidebar():
+#     return render_template('left-sidebar.html')
+
+# @app.route('/right-sidebar')
+# def right_sidebar():
+#     return render_template('right-sidebar.html')
+
+# @app.route('/no-sidebar')
+# def no_sidebar():
+#     return render_template('no-sidebar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
